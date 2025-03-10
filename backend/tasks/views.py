826a7caed8efc8449272ee1941e3c6a08db610c8
Katya-maslyanko <maslyankoy@list.ru>
2025-03-10 17:09:37 +0300

    Обновленние представлений

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,6 +1,40 @@
 from rest_framework import viewsets
-from .models import User, Team, Project, Task, Subtask, Comment, Notification, ActivityLog
-from .serializers import UserSerializer, TeamSerializer, ProjectSerializer, TaskSerializer, SubtaskSerializer, CommentSerializer, NotificationSerializer, ActivityLogSerializer, ProjectGoal, Subgoal
+from .models import (
+    User,
+    Team,
+    Project,
+    ProjectGoal,
+    Subgoal,
+    Task,
+    Subtask,
+    Tag,
+    TaskTag,
+    Comment,
+    Notification,
+    File,
+    Setting,
+    ActivityLog,
+    UserTeamRelation,
+    ProjectMember
+)
+from .serializers import (
+    UserSerializer,
+    TeamSerializer,
+    ProjectSerializer,
+    ProjectGoalSerializer,
+    SubgoalSerializer,
+    TaskSerializer,
+    SubtaskSerializer,
+    TagSerializer,
+    TaskTagSerializer,
+    CommentSerializer,
+    NotificationSerializer,
+    FileSerializer,
+    SettingSerializer,
+    ActivityLogSerializer,
+    UserTeamRelationSerializer,
+    ProjectMemberSerializer
+)
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -14,6 +48,14 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
+class ProjectGoalViewSet(viewsets.ModelViewSet):
+    queryset = ProjectGoal.objects.all()
+    serializer_class = ProjectGoalSerializer
+
+class SubgoalViewSet(viewsets.ModelViewSet):
+    queryset = Subgoal.objects.all()
+    serializer_class = SubgoalSerializer
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
@@ -21,6 +63,14 @@ class TaskViewSet(viewsets.ModelViewSet):
 class SubtaskViewSet(viewsets.ModelViewSet):
     queryset = Subtask.objects.all()
     serializer_class = SubtaskSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class TaskTagViewSet(viewsets.ModelViewSet):
+    queryset = TaskTag.objects.all()
+    serializer_class = TaskTagSerializer
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
@@ -30,14 +80,22 @@ class NotificationViewSet(viewsets.ModelViewSet):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
 
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+
+class SettingViewSet(viewsets.ModelViewSet):
+    queryset = Setting.objects.all()
+    serializer_class = SettingSerializer
+
 class ActivityLogViewSet(viewsets.ModelViewSet):
     queryset = ActivityLog.objects.all()
     serializer_class = ActivityLogSerializer
 
-class ProjectGoalViewSet(viewsets.ModelViewSet):
-    queryset = ProjectGoal.objects.all()
-    serializer_class = ProjectGoalSerializer
+class UserTeamRelationViewSet(viewsets.ModelViewSet):
+    queryset = UserTeamRelation.objects.all()
+    serializer_class = UserTeamRelationSerializer
 
-class SubgoalViewSet(viewsets.ModelViewSet):
-    queryset = Subgoal.objects.all()
-    serializer_class = SubgoalSerializer
+class ProjectMemberViewSet(viewsets.ModelViewSet):
+    queryset = ProjectMember.objects.all()
+    serializer_class = ProjectMemberSerializer
