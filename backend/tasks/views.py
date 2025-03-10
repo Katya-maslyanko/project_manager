60ad9891e8dd324d4998745f6e81852d0d31c406
Katya-maslyanko <maslyankoy@list.ru>
2025-03-10 16:53:09 +0300

    Создание представлений

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Team, Project, Task, Subtask, Comment, Notification, ActivityLog
+from .serializers import UserSerializer, TeamSerializer, ProjectSerializer, TaskSerializer, SubtaskSerializer, CommentSerializer, NotificationSerializer, ActivityLogSerializer, ProjectGoal, Subgoal
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class SubtaskViewSet(viewsets.ModelViewSet):
+    queryset = Subtask.objects.all()
+    serializer_class = SubtaskSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+class ActivityLogViewSet(viewsets.ModelViewSet):
+    queryset = ActivityLog.objects.all()
+    serializer_class = ActivityLogSerializer
+
+class ProjectGoalViewSet(viewsets.ModelViewSet):
+    queryset = ProjectGoal.objects.all()
+    serializer_class = ProjectGoalSerializer
+
+class SubgoalViewSet(viewsets.ModelViewSet):
+    queryset = Subgoal.objects.all()
+    serializer_class = SubgoalSerializer
