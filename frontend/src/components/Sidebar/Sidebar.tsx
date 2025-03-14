c9403ee46181e5de8f77eaf6a0444e0c067d9f37
Katya-maslyanko <maslyankoy@list.ru>
2025-03-14 18:01:55 +0300

    Изменения в Sidebar

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,18 @@
 "use client";
 import React, { useEffect, useRef, useState, useCallback } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { useSidebar } from "@/context/SidebarContext";
 import {
-  Grid,
-  User,
-  Calendar,
-  List,
+  LayoutDashboard,
+  CircleCheckBig,
+  ChartNoAxesCombined,
+  FolderKanban,
+  Users,
   ChevronDown,
+  PlusCircle,
 } from "lucide-react";
-import Image from "next/image";
 
 type NavItem = {
   name: string;
@@ -21,23 +23,23 @@ type NavItem = {
 
 const navItems: NavItem[] = [
   {
-    icon: <Grid />,
+    icon: <LayoutDashboard />,
     name: "Дашборд",
     path: "/",
   },
   {
-    icon: <User  />,
+    icon: <CircleCheckBig />,
     name: "Мои задачи",
     path: "/my-tasks",
   },
   {
-    icon: <Calendar />,
+    icon: <ChartNoAxesCombined />,
     name: "Отчет",
     path: "/report",
   },
   {
     name: "Проекты",
-    icon: <List />,
+    icon: <FolderKanban />,
     subItems: [
       { name: "Проект 1", path: "/project-1" },
       { name: "Проект 2", path: "/project-2" },
@@ -45,7 +47,7 @@ const navItems: NavItem[] = [
   },
   {
     name: "Группа",
-    icon: <List />,
+    icon: <Users />,
     subItems: [
       { name: "Группа 1", path: "/group-1" },
       { name: "Группа 2", path: "/group-2" },
@@ -57,183 +59,35 @@ const AppSidebar: React.FC = () => {
   const { isExpanded, isMobileOpen, isHovered, setIsHovered } = useSidebar();
   const pathname = usePathname();
 
-  const renderMenuItems = (
-    navItems: NavItem[],
-    menuType: "main" | "others"
-  ) => (
-    <ul className="flex flex-col gap-2">
-      {navItems.map((nav, index) => (
-        <li key={nav.name}>
-          {nav.subItems ? (
-            <button
-              onClick={() => handleSubmenuToggle(index, menuType)}
-              className={`flex items-center justify-between w-full px-3 py-2 text-gray-500 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-all duration-200 ease-in-out ${
-                openSubmenu?.type === menuType && openSubmenu?.index === index
-                  ? "bg-gray-100 dark:bg-gray-700"
-                  : ""
-              }`}
-            >
-              <div className="flex items-center">
-                <span className="mr-2">{nav.icon}</span>
-                {isExpanded && (
-                  <span className="text-sm">{nav.name}</span>
-                )}
-              </div>
-              <ChevronDown
-                className={`transition-transform duration-200 ${
-                  openSubmenu?.type === menuType && openSubmenu?.index === index
-                    ? "rotate-180"
-                    : ""
-                }`}
-              />
-            </button>
-          ) : (
-            nav.path && (
-              <Link
-                href={nav.path}
-                className={`flex items-center px-3 py-2 text-gray-500 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-all duration-200 ease-in-out ${
-                  isActive(nav.path) ? "bg-gray-100 dark:bg-gray-700" : ""
-                }`}
-              >
-                <span className="mr-2">{nav.icon}</span>
-                {isExpanded && (
-                  <span className="text-sm">{nav.name}</span>
-                )}
-              </Link>
-            )
-          )}
-          {nav.subItems && (isExpanded || isHovered || isMobileOpen) && (
-            <div
-              ref={(el) => {
-                subMenuRefs.current[`${menuType}-${index}`] = el;
-              }}
-              className="overflow-hidden transition-all duration-300"
-              style={{
-                height:
-                  openSubmenu?.type === menuType && openSubmenu?.index === index
-                    ? `${subMenuHeight[`${menuType}-${index}`]}px`
-                    : "0px",
-              }}
-            >
-              <ul className="mt-2 space-y-1 ml-4">
-                {nav.subItems.map((subItem) => (
-                  <li key={subItem.name}>
-                    <Link
-                      href={subItem.path}
-                      className={`flex items-center px-3 py-2 text-gray-500 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-all duration-200 ease-in-out ${
-                        isActive(subItem.path)
-                          ? "bg-gray-100 dark:bg-gray-700"
-                          : ""
-                      }`}
-                    >
-                      {subItem.name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </li>
-      ))}
-    </ul>
-  );
-
-  const [openSubmenu, setOpenSubmenu] = useState<{
-    type: "main" | "others";
-    index: number;
-  } | null>(null);
-  const [subMenuHeight, setSubMenuHeight] = useState<Record<string, number>>(
-    {}
-  );
-  const subMenuRefs = useRef<Record<string, HTMLDivElement | null>>({});
+  const [openProjects, setOpenProjects] = useState(false);
+  const [openGroups, setOpenGroups] = useState(false);
 
   const isActive = useCallback((path: string) => path === pathname, [pathname]);
 
-  useEffect(() => {
-    let submenuMatched = false;
-    ["main", "others"].forEach((menuType) => {
-      const items = menuType === "main" ? navItems : [];
-      items.forEach((nav, index) => {
-        if (nav.subItems) {
-          nav.subItems.forEach((subItem) => {
-            if (isActive(subItem.path)) {
-              setOpenSubmenu({
-                type: menuType as "main" | "others",
-                index,
-              });
-              submenuMatched = true;
-            }
-          });
-        }
-      });
-    });
+  const toggleProjects = () => {
+    setOpenProjects((prev) => !prev);
+  };
 
-    if (!submenuMatched) {
-      setOpenSubmenu(null);
-    }
-  }, [pathname, isActive]);
-
-  useEffect(() => {
-    if (openSubmenu !== null) {
-      const key = `${openSubmenu.type}-${openSubmenu.index}`;
-      if (subMenuRefs.current[key]) {
-        setSubMenuHeight((prevHeights) => ({
-          ...prevHeights,
-          [key]: subMenuRefs.current[key]?.scrollHeight || 0,
-        }));
-      }
-    }
-  }, [openSubmenu]);
-
-  const handleSubmenuToggle = (index: number, menuType: "main" | "others") => {
-    setOpenSubmenu((prevOpenSubmenu) => {
-      if (
-        prevOpenSubmenu &&
-        prevOpenSubmenu.type === menuType &&
-        prevOpenSubmenu.index === index
-      ) {
-        return null;
-      }
-      return { type: menuType, index };
-    });
+  const toggleGroups = () => {
+    setOpenGroups((prev) => !prev);
   };
 
   return (
     <aside
       className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
-        ${
-          isExpanded || isMobileOpen
-            ? "w-[290px]"
-            : "w-[90px]"
-        }
+        ${isExpanded || isMobileOpen ? "w-[290px]" : "w-[90px]"}
         ${isMobileOpen ? "translate-x-0" : "-translate-x-full"}
         lg:translate-x-0`}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
     >
-      <div
-        className={`py-7 flex  ${
-          !isExpanded && !isHovered ? "lg:justify-center" : "justify-start"
-        }`}
-      >
+      <div className={`py-7 flex ${!isExpanded ? "lg:justify-center" : "justify-start"}`}>
         <Link href="/">
           {isExpanded || isMobileOpen ? (
-            <>
-              <Image
-                className="dark:hidden"
-                src="/images/logo/icon_logo_open.svg"
-                alt="Icon Logo Open"
-                width={160}
-                height={60}
-              />
-              <Image
-                className="hidden dark:block"
-                src="/images/logo/icon_logo_dark.svg"
-                alt="Icon Logo Dark"
-                width={160}
-                height={60}
-              />
-            </>
+            <Image
+              src="/images/logo/icon_logo_open.svg"
+              alt="Icon Logo Open"
+              width={160}
+              height={60}
+            />
           ) : (
             <Image
               src="/images/logo/icon_logo_close.svg"
@@ -246,8 +100,98 @@ const AppSidebar: React.FC = () => {
       </div>
       <div className="flex flex-col overflow-y-auto duration-300 ease-linear no-scrollbar">
         <nav className="mb-6">
-          <div className="flex flex-col gap-2">
-            {renderMenuItems(navItems, "main")}
+          {/* Основные ссылки */}
+          <ul className="flex flex-col gap-2">
+            {navItems.slice(0, 3).map((nav) => (
+              <li key={nav.name}>
+                <Link
+                  href={nav.path!}
+                  className={`flex items-center px-4 py-3 text-gray-600 rounded-lg hover:bg-gray-200 transition-all duration-200 ease-in-out ${
+                    isActive(nav.path!) ? "bg-gray-300" : ""
+                  }`}
+                >
+                  <span className="mr-2 text-lg">{nav.icon}</span>
+                  {isExpanded && <span className="text-lg">{nav.name}</span>}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="border-t border-gray-300 my-4" />
+
+          {/* Мои Проекты */}
+          <div>
+            <div className="flex items-center justify-between">
+              <h2 className={`text-gray-400 text-sm font-semibold uppercase flex items-center ${!isExpanded ? "justify-center w-full" : ""}`}>
+                {isExpanded ? "Мои Проекты" : "..."}
+              </h2>
+              {isExpanded && (
+                <button className="bg-light-blue-200 rounded-lg p-1">
+                  <PlusCircle className="text-blue-600" />
+                </button>
+              )}
+            </div>
+            <button onClick={toggleProjects} className={`flex items-center justify-between w-full px-4 py-3 text-gray-600 rounded-lg hover:bg-gray-200 transition-all duration-200 ease-in-out ${openProjects ? "bg-gray-200" : ""}`}>
+              <span className="flex items-center">
+                <FolderKanban className="mr-2" />
+                {isExpanded && <span className="text-lg">Проекты</span>}
+              </span>
+              <ChevronDown className={`transition-transform duration-200 ${openProjects ? "rotate-180" : ""}`} />
+            </button>
+            {openProjects && (
+              <ul className="ml-4 mt-2 space-y-1">
+                {navItems[3].subItems?.map((subItem) => (
+                  <li key={subItem.name} className="flex items-center">
+                    <Link
+                      href={subItem.path!}
+                      className={`flex items-center w-full px-4 py-3 text-gray-600 rounded-lg hover:bg-gray-200 transition-all duration-200 ease-in-out ${
+                        isActive(subItem.path!) ? "bg-gray-300" : ""
+                      }`}
+                    >
+                      {subItem.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          <div className="border-t border-gray-300 my-4" />
+
+          {/* Мои Группы */}
+          <div>
+            <div className="flex items-center justify-between">
+              <h2 className={`text-gray-400 text-sm font-semibold uppercase flex items-center ${!isExpanded ? "justify-center w-full" : ""}`}>
+                {isExpanded ? "Мои Группы" : "..."}
+              </h2>
+              {isExpanded && (
+                <button className="bg-light-blue-200 rounded-lg p-1">
+                  <PlusCircle className="text-blue-600" />
+                </button>
+              )}
+            </div>
+            <button onClick={toggleGroups} className={`flex items-center justify-between w-full px-4 py-3 text-gray-600 rounded-lg hover:bg-gray-200 transition-all duration-200 ease-in-out ${openGroups ? "bg-gray-200" : ""}`}>
+              <span className="flex items-center">
+                <Users className="mr-2" />
+                {isExpanded && <span className="text-lg">Группы</span>}
+              </span>
+              <ChevronDown className={`transition-transform duration-200 ${openGroups ? "rotate-180" : ""}`} />
+            </button>
+            {openGroups && (
+              <ul className="ml-4 mt-2 space-y-1">
+                {navItems[4].subItems?.map((subItem) => (
+                  <li key={subItem.name} className="flex items-center">
+                    <Link
+                      href={subItem.path!}
+                      className={`flex items-center w-full px-4 py-3 text-gray-600 rounded-lg hover:bg-gray-200 transition-all duration-200 ease-in-out ${
+                        isActive(subItem.path!) ? "bg-gray-300" : ""
+                      }`}
+                    >
+                      {subItem.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
           </div>
         </nav>
       </div>
